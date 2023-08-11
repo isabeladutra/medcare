c24fd4c6a149c1481622325390928b90acaaf070
isabeladutra <dutraisabela88@gmail.com>
2023-08-11 12:48:29 -0300

    cors config

--- src/main/java/br/com/medcare/configs/CorsConfig.java
+++ src/main/java/br/com/medcare/configs/CorsConfig.java
@@ -1,19 +1,21 @@
 package br.com.medcare.configs;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-public class CorsConfig implements CorsConfigurationSource{
+@Configuration
+public class CorsConfig implements CorsConfigurationSource {
 	@Override
-    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
-        return config;
-    }
+	public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		config.setAllowCredentials(true);
+		return config;
+	}
 
 }
